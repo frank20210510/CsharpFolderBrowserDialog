38dc2b7abbf98942529e6c867b1dbd339dbb6470
mikeryzhov <mikeryzhov@FRANKMIKE>
2021-06-23 17:22:46 +0800

    Add project files.

---
+++ CsharpFolderBrowserDialog/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsharpFolderBrowserDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            fbd.Description = "+++Select Folder+++";
+            fbd.ShowNewFolderButton = false;
+
+            if(fbd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = fbd.SelectedPath;
+            }
+
+        }
+    }
+}
